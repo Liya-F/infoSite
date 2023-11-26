df6543d62bbc095928886c7b8a3467d4bfa93d98
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2023-11-25 18:19:03 -0800

    font changes

--- src/pages/Home/heroSection.jsx
+++ src/pages/Home/heroSection.jsx
@@ -42,14 +42,14 @@ const HeroSection = () => {
   };
 
   return (
-    <div className="relative h-[80vh]" style={heroSectionStyle}>
+    <div className="relative h-[100vh] pt-10" style={heroSectionStyle}>
       <div className="absolute inset-0 bg-gradient-to-r from-primary to-transparent"></div>
       <div className="relative flex items-center justify-start min-h-screen">
         <div className="container  md:mx-20 mx-10 w-auto md:mt-[-100px] mt-[-150px] ">
-          <h1 className="text-6xl md:max-w-4xl text-white font-bold mb-8 relative z-10 md:text-left text-center">
+          <h1 className="md:text-6xl text-5xl md:max-w-4xl text-white font-bold mb-8 relative z-10 md:text-left text-center">
             Building a Brighter Future Together
           </h1>
-          <p className="text-white text-lg mb-8 relative z-10 md:w-1/2">
+          <p className="text-white md:text-lg text-md mb-8 relative z-10 md:w-1/2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget risus mauris. Lorem ipsum dolor sit
             amet, consectetur adipiscing elit. Vestibulum eget risus mauris.
           </p>
